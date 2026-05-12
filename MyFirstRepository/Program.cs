e1e747031a444930f85dcd76f288772348a5f448
tunnelsensor <senne.tilkin@gmail.com>
2026-05-12 14:13:50 +0200

    Version1

---
+++ MyFirstRepository/Program.cs
@@ -0,0 +1,51 @@
+﻿Zombie zombie1 = new Zombie(10);
+
+zombie1.damage(1);
+zombie1.showStats();
+zombie1.heal(2);
+zombie1.showStats();
+zombie1.damage(200);
+zombie1.showStats();
+
+class Zombie
+{
+    int maxHP = 0;
+    int hp = 0;
+
+    public Zombie(int maxhp)
+    {
+        this.maxHP = maxhp;
+        this.hp = maxhp;
+    }
+
+    public void heal(int healHP)
+    {
+        hp += healHP;
+        if (hp > maxHP)
+        {
+            hp = maxHP;
+        }
+    }
+    public void damage(int damage)
+    {
+        hp -= damage;
+        if (hp < 0)
+        {
+            dead();
+        }
+    }
+
+    public void dead()
+    {
+        Console.WriteLine("Dead");
+    }
+
+    public void showStats()
+    {
+        Console.WriteLine("------------------");
+        Console.WriteLine("MaxHP: " + maxHP);
+        Console.WriteLine("Hp: " + hp);
+        Console.WriteLine("------------------");
+
+    }
+}
